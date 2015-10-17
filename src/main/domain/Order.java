e57aac824c5231752375d8b12e73b83d67b9d67f
mzweigert <mateuszzweigert@gmail.com>
2015-10-17 23:42:44 +0200

    Added Order class

---
+++ src/main/domain/Order.java
@@ -0,0 +1,55 @@
+package main.domain;
+
+import java.sql.Date;
+
+public class Order
+{
+
+	private int idOrder;
+	private int idBook, idReader;
+	private Date orderDate;
+
+	
+	public Order(int idOrder, int idBook, int idReader, Date orderDate)
+	{
+		this.idOrder = idOrder;
+		this.idBook = idBook;
+		this.idReader = idReader;
+		this.orderDate = orderDate;
+	}
+
+
+
+	public int getIdBook() {
+		return idBook;
+	}
+
+	public void setIdBook(int idBook) {
+		this.idBook = idBook;
+	}
+
+	public int getIdReader() {
+		return idReader;
+	}
+
+	public void setIdReader(int idReader) {
+		this.idReader = idReader;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public int getIdOrder() {
+		return idOrder;
+	}
+
+	public void setIdOrder(int idOrder) {
+		this.idOrder = idOrder;
+	}
+
+}
