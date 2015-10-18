1e8f382fcf2db5786efb175b7fc7fa43b0e056de
mzweigert <mateuszzweigert@gmail.com>
2015-10-18 17:15:18 +0200

    Added getBookById, getBookByTitle,  updateBook and deleteBook methods in BookManager service

--- src/main/BookDAO.java
+++ src/main/BookDAO.java
@@ -10,8 +10,12 @@ import java.util.List;
 public interface BookDAO
 {
 
-    int addBook(Book book);
     List<Book> getAllBooks();
+    Book getBookById(int idBook);
+    List<Book> getBookByTitle(String title);
+    boolean updateBook(Book book);
+    boolean deleteBook(Book book);
+    boolean addBook(Book book);
 
 
 }
